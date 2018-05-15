2ecc216ddfd466e99b6df7ce102b1d0c5c4c58e5
liormiz <liorstrongxd>
2018-05-15 20:32:36 +0300

    add enums

---
+++ Basket.Common/Basket.Common/Enums/eCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Common.Enums
+{
+    public enum eCategory
+    {
+        MilkAndEggs = 1,
+        Bread = 2,
+        FruitAndVegs = 3,
+        MeatAndFish = 4,
+        Candies = 5,
+        Dganim = 6,
+        Freeze = 7,
+        Cook = 8,
+        Clean = 9,
+        HomePets = 10,
+        HealthAndOrganic = 11,
+        Babies = 12
+    }
+}
